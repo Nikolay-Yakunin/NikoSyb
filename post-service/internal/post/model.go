c8e016aa75df3fa4a9525693ed0b563c864b617c
Nikolay-Yakunin <akuninn52@gmail.com>
2025-11-07 22:07:36 +0700

    refact model and add New

--- post-service/internal/post/model.go
+++ post-service/internal/post/model.go
@@ -1,13 +1,29 @@
+// post -Package
 package post
 
 import (
-	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
+/*
+Post - структура поста
+
+Пост имеет заголовок, тело поста, это md разметка, и набор тегов.
+*/
 type Post struct {
 	gorm.Model
-	Title  string         `json:"title" binding:"required" gorm:"type:varchar(255);not null"`
-	Body   string         `json:"body" binding:"required" gorm:"type:text;not null"`
-	Images pq.StringArray `json:"images" gorm:"type:text[]"`
+	Title string   `json:"title" binding:"required" gorm:"type:varchar(255);not null"`
+	Body  string   `json:"body" binding:"required" gorm:"type:text;not null"`
+	Tags  []string `json:"tags" binding:"required" gorm:"type:text[];not null"`
+}
+
+// New - генератор
+func New(title, body string, tags []string) *Post {
+	var post Post
+
+	post.Title = title
+	post.Body = body
+	post.Tags = tags
+
+	return &post
 }
